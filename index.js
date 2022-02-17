ade43069cebd911c494f3bbb624c1c0c0af6f8b6
hashiong <hongenl@uci.edu>
2022-02-16 19:14:25 -0800

    integrate build folder to backend

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
-
+app.use(express.static('build'))
 app.use(express.json())
 const cors = require('cors')
 app.use(cors())
+
 
 let notes = [
       {
