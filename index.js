a6759f34399abf16fc56051f2370beb16e3a6ac4
hashiong <hongenl@uci.edu>
2022-03-19 21:19:43 -0700

    bug fixed

--- index.js
+++ index.js
@@ -1,87 +1,12 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-require('dotenv').config()
-const Note = require('./models/note')
-const { response } = require('express')
+const server = http.createServer(app)
 
-app.use(express.static('build'))
-app.use(express.json())
-app.use(cors())
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        if(note){
-            response.json(note)
-        }else{
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
-})
 
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-    if (body.content === undefined){
-        return response.status(400).json({error: 'content missing'})
-    }
-    const note = new Note({
-        content:body.content,
-        important:body.important || false,
-        date: new Date(),
-    })
-
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
-    .catch(error => next(error))
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-    const {content, important} = request.body
-
-    Note.findByIdAndUpdate(request.params.id, {content, important}, {new: true, runValidators: true, context: 'query'})
-    .then(updateNote => {
-        response.json(updatedNote)
-    })
-    .catch(error => next(error))
-})
-
-const unkownEndpoint = (request, response) => {
-    response.status(404).send({error: "unknown endpoint"})
-}
-
-app.use(unkownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-
-    if(error.name === 'CastError'){
-        return response.status(400).send({error: 'malformatted id'})
-    }else if(error.name === 'ValidationError'){
-        return response.status(400).json({error: error.message})
-    }
-
-    next(error)
-}
-
-app.use(errorHandler)
-
-
-
-const PORT = process.env.PORT
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
